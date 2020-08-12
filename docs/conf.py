0db7497a052e9e361ece48af836ddf861b00df04
Clara Burgard <clara.burgard@gmail.com>
2020-08-12 16:31:14 +0200

    added refs and intersphinx.mapping for easier referencing

--- docs/conf.py
+++ docs/conf.py
@@ -69,3 +69,11 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     # relative to this directory. They are copied after the builtin static files,
     # so a file named "default.css" will overwrite the builtin "default.css".
     html_static_path = ['_static']
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3.6/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'xarray': ('http://xarray.pydata.org/en/stable/', None),
+    }
